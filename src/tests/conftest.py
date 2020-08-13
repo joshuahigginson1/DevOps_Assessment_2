cbe42985a985b837751805185d2a512bb44f028b
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-14 00:27:18 +0100

    QDA2-58 - Create random number generator function, from 1 - 12 + a rest value. : #comment Full test coverage of function. New PyTest Fixture for all common scales. New conftest file. Updated requirements file.

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -4,6 +4,10 @@
 
 # “The conftest.py file gets read by pytest, and is considered a local plugin.
 
+# Imports ---------------------------------------------------------------------------------
+
+import pytest
+
 # Fixtures --------------------------------------------------------------------------------
 
 """@pytest.fixture(name="connect_database", scope='module', autouse=False)
@@ -15,3 +19,20 @@ def tasks_db_connection(tmpdir):
 
     # Teardown : stop db
     tasks.stop_tasks_db()"""
+
+
+# Fixtures are a great place to store data to use for testing. You can return anything.
+
+@pytest.fixture(name='common_scales', scope='function', autouse=False)
+def common_scales():  # All of the common scales. In tab, this would be all scales with the root note of 'F'.
+    common_scales = {
+        "chromatic": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+        "major": [1, 3, 5, 6, 8, 10, 12, 13],
+        "major pentatonic": [1, 3, 5, 8, 10, 13],
+        "major blues": [1, 3, 4, 5, 8, 10, 13],
+        "natural minor": [1, 3, 4, 6, 8, 9, 11, 13],
+        "harmonic minor": [1, 3, 4, 6, 8, 9, 12, 13],
+        "minor pentatonic": [1, 4, 6, 8, 11, 13],
+        "minor blues": [1, 4, 6, 7, 8, 11, 13]
+    }
+    return common_scales
--- src/tests/test_service2.py
+++ src/tests/test_service2.py
@@ -4,6 +4,7 @@
 
 from src.service2.service2 import random_note_pitch
 
+
 # Classes --------------------------------------------------------------------------------
 
 
@@ -12,14 +13,13 @@ from src.service2.service2 import random_note_pitch
 
 # Methods --------------------------------------------------------------------------------
 
-chromatic_scales = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-
 
 # Define Variables -----------------------------------------------------------------------
 
-
 # Execute Code ---------------------------------------------------------------------------
-def test_random_note_pitch():
-    note = random_note_pitch(chromatic_scales)
-    assert note >= 0
-    assert isinstance(note, int) is True
+
+def test_random_note_pitch(common_scales):
+    for key, scales in common_scales.items():  # For every scale in our fixture of common scales, run our assertion.
+        note = random_note_pitch(scales)
+        assert 0 <= note <= 13  # Cannot be out of an octave bounds.
+        assert isinstance(note, int) is True  # Cannot be a data type other than an integer.
