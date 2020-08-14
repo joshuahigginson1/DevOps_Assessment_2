53d82b30adc05a93327ee49f3f7ff08cc3a65a6a
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-14 23:08:01 +0100

    QDA2-80 - Automate test report coverage with cobertura from py-cov xml : #comment Updated some syntax. Added venv dependencies. Create new venv on vm.

--- dependencies/jenkins_test_runner.bash
+++ dependencies/jenkins_test_runner.bash
@@ -1,8 +1,14 @@
 # A bash script in order to run my test dependencies.
 
-# Activate our virtual environment.
+# Update dependencies.
+sudo apt-get update
+sudp apt-get install -y python3 python3-pip python3-venv
 
-. ./venv/bin/activate
+# Create our virtual environment.
+python3 -m venv venv
+
+# Activate our virtual environment.
+source /venv/bin/activate
 
 # Install our project requirements.
 sudo pip3 install -r dependencies/requirements.txt
