90b2368f72629e42414df61975fc7148d36cf1f1
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-14 22:48:06 +0100

    Started Jenkins Test Runner Files. Opened new JIRA task.

---
+++ dependencies/jenkins_test_runner.bash
@@ -0,0 +1,16 @@
+# A bash script in order to run my test dependencies.
+
+# Here, I declare my paths.
+code_path=./src
+venv_path=./venv
+test_dir=./src/tests
+test_results_dir=./src/tests/test_results
+
+# Activate our virtual environment.
+source ${venv_path}/bin/activate
+
+# Install our project requirements.
+sudo pip3 install -r ./dependencies/requirements.txt
+
+# Run our test suite.
+pytest --junit-xml=${test_results_dir}/results.xml
