c2167a1dcead1861890dcad3fbfc689f1aa7ab30
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-14 23:00:21 +0100

    QDA2-80 - Automate test report coverage with cobertura from py-cov xml : #comment Updated some syntax. Not entirely sure that this one will work either.

--- dependencies/jenkins_test_runner.bash
+++ dependencies/jenkins_test_runner.bash
@@ -1,17 +1,11 @@
 # A bash script in order to run my test dependencies.
 
-# Here, I declare my paths.
-code_path=./src
-venv_path=./venv
-test_dir=/src/tests
-test_results_dir=./src/tests/test_results
-
 # Activate our virtual environment.
-#
-# source venv/bin/activate
+
+source venv/bin/activate
 
 # Install our project requirements.
-sudo pip3 install -r ./dependencies/requirements.txt
+sudo pip3 install -r dependencies/requirements.txt
 
 # Run our test suite.
-pytest --junit-xml=${test_results_dir}/results.xml
+pytest --junit-xml=src/tests.test_results/results.xml
