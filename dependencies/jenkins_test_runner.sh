745bd1a1659d18fa116ef08e706bd54f1ddb2eb0
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-15 00:53:27 +0100

    QDA2-80 - Automate test report coverage with cobertura from py-cov xml : Removed progress so far. I don't think that it's the correct way to cover lint violation reports. We can come back some other time!!

--- dependencies/jenkins_test_runner.sh
+++ dependencies/jenkins_test_runner.sh
@@ -13,11 +13,5 @@ source /venv/bin/activate
 # Install our project requirements.
 sudo pip3 install -r dependencies/requirements.txt
 
-sudo pylint src
-
-# Run our linter on our source finder, the output that data to 'pylint.blog'
-
-sudo pylint --output-format=pylint_junit.JUnitReporter src/
-
 # Run our test suite.
 sudo pytest --junit-xml=src/tests/test_results/junit_results.xml
