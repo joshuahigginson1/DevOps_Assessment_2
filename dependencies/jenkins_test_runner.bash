99573596be44d94eb986ec71ed2b44cb74d5f6fb
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-14 23:14:34 +0100

    QDA2-80 - Automate test report coverage with cobertura from py-cov xml : #comment Googled fix.

--- dependencies/jenkins_test_runner.bash
+++ dependencies/jenkins_test_runner.bash
@@ -1,5 +1,11 @@
 # A bash script in order to run my test dependencies.
 
+# Fix for "pyenv not working" error, caused by locales.
+
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+sudo dpkg-reconfigure locales
+
 # Update dependencies.
 sudo ap-get update
 sudo apt-get install -y python3 python3-pip python3-venv
