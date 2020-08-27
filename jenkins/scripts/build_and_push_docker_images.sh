5b49300a0ff0e142fa41a64073ebb792959e3fb2
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-28 00:23:44 +0100

    QDA2-54 - Ansible - Configuration Management : #comment trying to fix bug - can't CD into dir?

--- jenkins/scripts/build_and_push_docker_images.sh
+++ jenkins/scripts/build_and_push_docker_images.sh
@@ -7,18 +7,18 @@ docker login -u"$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 pwd
 ls
 
-sudo cd ~/DevOps_Assessment_2/service1
+cd service1
 docker build -t joshuahigginson1/melodie_service1:latest .
 docker push joshuahigginson1/melodie_service1:latest
 
-sudo cd ~/DevOps_Assessment_2/service2
+cd service2
 docker build -t joshuahigginson1/melodie_service2:latest .
 docker push joshuahigginson1/melodie_service2:latest
 
-sudo cd ~/DevOps_Assessment_2/service3
+cd service3
 docker build -t joshuahigginson1/melodie_service3:latest .
 docker push joshuahigginson1/melodie_service3:latest
 
-sudo cd ~/DevOps_Assessment_2/service4
+cd service4
 docker build -t joshuahigginson1/melodie_service4:latest .
 docker push joshuahigginson1/melodie_service4:latest
