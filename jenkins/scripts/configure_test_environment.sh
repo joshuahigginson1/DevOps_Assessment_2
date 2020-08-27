fbe70a325f5b0f4e10e4f228eb399ac874d040f9
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-27 21:49:22 +0100

    QDA2-54 - Ansible - Configuration Management : #comment Added shebangs

--- jenkins/scripts/configure_test_environment.sh
+++ jenkins/scripts/configure_test_environment.sh
@@ -3,15 +3,15 @@
 # A script to configure our test environment.
 
 # Update dependencies.
-apt update
-apt upgrade
-apt install -y python3 python3-pip python3-venv lilypond gunicorn
+sudo apt update
+sudo apt upgrade
+sudo apt install -y python3 python3-pip python3-venv lilypond gunicorn
 
 # Create our virtual environment.
-python3 -m venv venv
+sudo python3 -m venv venv
 
 # Activate our virtual environment.
 source /venv/bin/activate
 
 # Install our project requirements.
-pip3 install -r jenkins/test_env/test_env_requirements.txt
+sudo pip3 install -r jenkins/test_env/test_env_requirements.txt
