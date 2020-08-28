1e6a040d4ec29892e697ecaf7c177eb8fa25dee7
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-28 12:09:03 +0100

    QDA2-54 - Ansible - Configuration Management : #comment Correcting errors in docker-compose.yaml

--- jenkins/scripts/build_and_push_docker_images.sh
+++ jenkins/scripts/build_and_push_docker_images.sh
@@ -5,18 +5,22 @@
 docker login -u"$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 
 cd service1
+pwd
 docker build -t joshuahigginson1/melodie_service1:latest .
 docker push joshuahigginson1/melodie_service1:latest
 
 cd service2
+pwd
 docker build -t joshuahigginson1/melodie_service2:latest .
 docker push joshuahigginson1/melodie_service2:latest
 
 cd service3
+pwd
 docker build -t joshuahigginson1/melodie_service3:latest .
 docker push joshuahigginson1/melodie_service3:latest
 
 cd service4
+pwd
 docker build -t joshuahigginson1/melodie_service4:latest .
 docker push joshuahigginson1/melodie_service4:latest
 
