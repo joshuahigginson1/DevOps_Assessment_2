e8c11955b6f7edbb3ae6eab012846421093dcf0c
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-28 10:07:16 +0100

    QDA2-54 - Ansible - Configuration Management : #comment Adding back test reports to the project.

--- jenkins/scripts/deploy_docker_swarm_stack.sh
+++ jenkins/scripts/deploy_docker_swarm_stack.sh
@@ -4,11 +4,11 @@
 
 # Need to install all of the requirements to run python3.8, pytest, etc.
 
-scp docker-compose.yaml jenkins@melodie-manager-1:/
+scp docker-compose.yaml jenkins@melodie-manager-1:/home/jenkins
 
 ssh jenkins@melodie-manager-1 << EOF
 echo "This machine is currently being controlled by Jenkins-Ansible-Driver."
-pwd
+ls
 docker stack deploy --compose-file docker-compose.yaml melodie-stack
 EOF
 
