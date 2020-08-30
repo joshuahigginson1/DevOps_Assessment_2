7951d37c04cb182a327e81e81b10aa04fcfcab38
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-30 16:38:32 +0100

    QDA2-54 - Ansible - Configuration Management : #comment Debugging issues with Ansible, jenkins, running tests.

--- jenkins/scripts/run_test_service_4.sh
+++ jenkins/scripts/run_test_service_4.sh
@@ -13,7 +13,7 @@ export MIDI_DIRECTORY="$MIDI_DIRECTORY"
 
 sudo rm test_results/*
 
-python3 -m pytest -v --cov=src --cov-report term-missing --junit-xml=test_results/test_results_service4.xml
+python3 -m pytest -v --cov=src --cov-report term-missing xml:test_cov_service1.xml --junit-xml=test_results/test_results_service4.xml
 
 printf "\n"
 printf "\n"
