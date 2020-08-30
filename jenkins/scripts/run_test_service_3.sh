a404e5d5b285be3da4aa65a6c00b98a8bb7f513c
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-30 15:42:35 +0100

    QDA2-54 - Ansible - Configuration Management : #comment Debugging issues with Ansible, jenkins, running tests.

--- jenkins/scripts/run_test_service_3.sh
+++ jenkins/scripts/run_test_service_3.sh
@@ -10,7 +10,7 @@ export FLASK_ENV="$FLASK_ENV"
 
 sudo rm test_results/*
 
-python3 -m pytest -v --junit-xml=test_results/test_results_service3.xml
+python3 -m pytest --cov -v --junit-xml=test_results/test_results_service3.xml
 
 printf "\n"
 printf "\n"
