60bf5ad1823514aeb45aef7e45ff71ea64816eac
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-30 14:10:32 +0100

    QDA2-54 - Ansible - Configuration Management : #comment Debugging issues with Ansible, jenkins, running tests.

--- jenkins/scripts/run_test_service_1.sh
+++ jenkins/scripts/run_test_service_1.sh
@@ -29,9 +29,9 @@ printf "\n"
 
 # So instead, we use the 'pylint-fail-under' plugin.
 
-sudo sh -c "pylint-fail-under --fail_under 1 -f parseable src > test_results/service1_pylint_report.log"
+pylint-fail-under --fail_under 1 -f parseable src > test_results/service1_pylint_report.log
 
 # Run our Pep8 function, and saves the output to a new .txt file.
 # We just want the style report, not for the program to fail our build.
 
-sudo sh -c "pycodestyle src > test_results/service1_pep8_report.txt || exit 0"
+pycodestyle src > test_results/service1_pep8_report.txt || exit 0
--- jenkins/scripts/run_test_service_2.sh
+++ jenkins/scripts/run_test_service_2.sh
@@ -20,9 +20,9 @@ printf "\n"
 
 # So instead, we use the 'pylint-fail-under' plugin.
 
-sudo sh -c "pylint-fail-under --fail_under 1 -f parseable src > test_results/service2_pylint_report.log"
+pylint-fail-under --fail_under 1 -f parseable src > test_results/service2_pylint_report.log
 
 # Run our Pep8 function, and saves the output to a new .txt file.
 # We just want the style report, not for the program to fail our build.
 
-sudo sh -c "pycodestyle src > test_results/service2_pep8_report.txt || exit 0"
+pycodestyle src > test_results/service2_pep8_report.txt || exit 0
