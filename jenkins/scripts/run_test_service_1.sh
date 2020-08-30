0325235fcc973439ac65957b6bc65322d657210d
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-30 14:35:19 +0100

    QDA2-54 - Ansible - Configuration Management : #comment Debugging issues with Ansible, jenkins, running tests.

--- jenkins/scripts/run_test_service_1.sh
+++ jenkins/scripts/run_test_service_1.sh
@@ -31,17 +31,13 @@ printf "\n"
 
 # So instead, we use the 'pylint-fail-under' plugin.
 
-sudo pylint-fail-under --fail_under 1 -f parseable src > test_results/service1_pylint_report.log
-
 echo 'Writing PYLINT Report...'
 
-cat test_results/service1_pylint_report.log
+sudo pylint-fail-under --fail_under 1 -f parseable src > test_results/service1_pylint_report.log
 
 # Run our Pep8 function, and saves the output to a new .txt file.
 # We just want the style report, not for the program to fail our build.
 
-sudo pycodestyle /var/lib/jenkins/workspace/melodie-pipeline/service1/src > test_results/service1_pep8_report.txt || exit 0
-
 echo 'Writing PEP8 Report...'
 
-cat test_results/service1_pep8_report.txt
+sudo pycodestyle /var/lib/jenkins/workspace/melodie-pipeline/service1/src > test_results/service1_pep8_report.txt || exit 0
