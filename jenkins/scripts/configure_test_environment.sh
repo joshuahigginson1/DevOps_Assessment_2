8e36277804a4db6570ed329d215d49c97d445d74
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-31 20:42:01 +0100

    QDA2-54 - Ansible - Configuration Management : #comment Debugging issues with Ansible, jenkins, running tests.

--- jenkins/scripts/configure_test_environment.sh
+++ jenkins/scripts/configure_test_environment.sh
@@ -5,7 +5,7 @@
 # Update dependencies.
 sudo apt update
 sudo apt upgrade -y
-# sudo apt install -y python3 python3-pip python3-venv lilypond gunicorn
+sudo apt install -y python3 python3-pip python3-venv lilypond gunicorn
 
 # Create our virtual environment.
 # sudo python3 -m venv venv
