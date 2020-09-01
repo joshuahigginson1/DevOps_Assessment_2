f670367b027d2c8b8334a7ce25cbd93aa15c0d5e
Josh Higginson <joshuahigginson1@gmail.com>
2020-09-01 01:35:57 +0100

    QDA2-54 - Ansible - Configuration Management : #comment Final additions to the documentation.

--- service3/src/tests/conftest.py
+++ service3/src/tests/conftest.py
@@ -31,7 +31,8 @@ def common_rhythms():
         "short": [4, 8, 16, 32],
         "long": [1, 2, 4],
         "standard": [2, 4, 8, 16],
-        "extremes": [1, 32, 64, 128]
+        "extremes": [1, 32, 64, 128],
+        "midi implementation": [2, 4, 8, 16]
     }
 
     return common_rhythms_dictionary
