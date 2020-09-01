eb8aaa46c883eac76b92d961eb7b503f564441d4
Josh Higginson <joshuahigginson1@gmail.com>
2020-09-01 10:16:53 +0100

    Final Submission

--- service3/src/service3.py
+++ service3/src/service3.py
@@ -51,7 +51,7 @@ def return_rhythms_dictionary():
         "long": [1, 2, 4],
         "standard": [2, 4, 8, 16],
         "extremes": [1, 32, 64, 128],
-        "midi implementation": [2, 4, 8, 16]
+        "png implementation": [4, 8, 16, 32]
     }
 
     return rhythms_dictionary
